b1121bea65ec9abb41d85d47e64a10d7a8bf1c99
Konrad1991 <konrad_kraemer@yahoo.de>
2024-05-24 16:30:14 +0200

    changed the type of Vec<T, unaryOperation> to Vec<ExtractedDataType...>

--- tests/Unary_Tests.cpp
+++ tests/Unary_Tests.cpp
@@ -11,26 +11,18 @@ void test_arithmetic() {
     Vec<double> a = coca(1, 2, 3);
     Vec<double> b = coca(4, 5, 6);
     Vec<double> c;
-    print(-a);
-    print(sinus(a));    // 0.8414710 0.9092974 0.1411200
-    print(asinus(a));   // 1.570796      NaN      NaN
-    print(sinush(a));   //  1.175201  3.626860 10.017875
-    print(cosinus(a));  // 0.5403023 -0.4161468 -0.9899925
-    print(acosinus(a)); // 0, NaN  NaN
-    print(cosinush(a)); //  1.543081  3.762196 10.067662
-    print(tangens(a));  //  1.5574077 -2.1850399 -0.1425465
-    print(atangens(a)); //  0.7853982 1.1071487 1.2490458
-    print(tangensh(a)); // 0.7615942 0.9640276 0.9950548
-    print(ln(a));       // 0.0000000 0.6931472 1.0986123
-    print(sqroot(a));   // 1.000000 1.414214 1.732051
-    print(exp(a));      //   2.718282  7.389056 20.085537
-
-    print((a + 1) == coca(2, 3, 4));
-    print(sinus(a) == 3); // TODO: fix
-    print(sinus(a) == a);
-    print((a + a) == a);
-    printTAST<decltype(sinus(a))>();
-    printTAST<decltype(a + a)>();
+    ass(sinus(a) == coca(0.8414710, 0.9092974, 0.1411200), s + "sinus");
+    ass(asinus(a) == coca(1.570796, NA, NA), s + "asinus");
+    ass(sinush(a) == coca(1.175201, 3.626860, 10.017875), s + "sinush");
+    ass(cosinus(a) == coca(0.5403023, -0.4161468, -0.9899925), s + "cosinus");
+    ass(acosinus(a) == coca(0, NA, NA), s + "acosinus");
+    ass(cosinush(a) == coca(1.543081, 3.762196, 10.067662), s + "cosinush");
+    ass(tangens(a) == coca(1.5574077, -2.1850399, -0.1425465), s + "tangens");
+    ass(atangens(a) == coca(0.7853982, 1.1071487, 1.2490458), s + "atangens");
+    ass(tangensh(a) == coca(0.7615942, 0.9640276, 0.9950548), s + "tangensh");
+    ass(ln(a) == coca(0.0000000, 0.6931472, 1.0986123), s + "ln");
+    ass(sqroot(a) == coca(1.000000, 1.414214, 1.732051), s + "sqrt");
+    ass(exp(a) == coca(2.718282, 7.389056, 20.085537), s + "exp");
   }
 }
 
